73a00b63a5065d7f02dccac6dbb9ede11055a75a
Vera Barysava <Vera_Barysava@epam.com>
2018-02-05 19:51:42 +0300

    Node js part 2

--- routes.js
+++ routes.js
@@ -1,16 +1,16 @@
-module.exports = function(app){
-
-	let blogs = require('./controllers/blogs');
-
-	app.get('/blogs', blogs.findAll); 
-	app.get('/blogs/:id', blogs.findById); 
-  app.put('/blogs/:id', blogs.update);
-  app.post('/blogs', blogs.add);
-  app.delete('/blogs/:id', blogs.delete);
-	app.get('/import', blogs.import);  
-
-	app.get('/hello', function(req, res) {
-	    res.send('Hello!\n');
-	});
-
+module.exports = function(app) {
+    let blogs = require('./controllers/blogs');
+    let auth = require('./controllers/AuthController.js');
+    app.get('/blogs', blogs.findAll);
+    app.get('/blogs/:id', blogs.findById);
+    app.put('/blogs/:id', blogs.update);
+    app.post('/blogs', blogs.add);
+    app.delete('/blogs/:id', blogs.delete);
+    app.get('/import', blogs.import);
+    app.get('/', auth.home);
+    app.get('/register', auth.register);
+    app.post('/register', auth.doRegister);
+    app.get('/login', auth.login);
+    app.post('/login', auth.doLogin);
+    app.get('/logout', auth.logout);
 };
--- server.js
+++ server.js
@@ -1,28 +1,45 @@
-let express = require('express'),
-mongoose = require('mongoose'),
-fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy; // Username and password authentication strategy for Passport and Node.js.
 
-let mongoUri = 'mongodb://localhost/noderest';
-mongoose.connect(mongoUri);
-let db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + mongoUri);
-});
+let mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+mongoose.connect('mongodb://localhost/node-auth')
+    .then(() => console.log('connection succesful'))
+    .catch((err) => console.error(err));
+
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'app/access.log'), {flags: 'a'});
 let app = express();
-
-/*let app.configure(function(){
-  
-});
-*/
 
 require('./models/BlogModel');
 require('./routes')(app);
+
+app.use(logger('combined', {stream: accessLogStream}));
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'pug');
 app.listen(3001);
 app.use(function(req, res, next) {
-    if(req.accepts('html') && res.status(404)) {
+    if (req.accepts('html') && res.status(404)) {
         res.render('index.pug');
         return;
-    }});
-console.log('Listening on port 3001...');
+    }
+});
+
+let User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
